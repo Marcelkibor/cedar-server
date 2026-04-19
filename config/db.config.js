23b6944743adaf799db4fa516692771db8a6f52d
Marcel <kibormarcel90@gmail.com>
2026-04-19 17:44:45 +0300

    force db

--- config/db.config.js
+++ config/db.config.js
@@ -13,21 +13,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
   }
 });
-// const sequelize = new Sequelize
-// ({
-//   database: process.env.DB_NAME,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: 'postgres',
-//   logging: false,
-//   define: {
-//     freezeTableName: true
-//   },
-// });
 
-sequelize.sync({ alter: true })
+
+sequelize.sync({ force: true })
   .then(() => console.log('DB synced successfully.'))
   .catch(err => console.error('DB sync error:', err));
 
