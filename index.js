76fcd2a8df38aa88c181d5eabeb822ee712e6b2f
Marcel <kibormarcel90@gmail.com>
2026-04-20 10:53:37 +0300

    cors fix

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-const routes = require("./routes/apis");
 const express = require("express");
-const dbConnect  = require('./config/db.config');
-dbConnect.sync()
+const cors = require("cors");
+const routes = require("./routes/apis");
+const dbConnect = require("./config/db.config");
+
+dbConnect.sync();
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://cedarclinic.co.ke",
+    "https://www.cedarclinic.co.ke"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api", routes);
-const cors = require('cors');
-const port = process.env.PORT || 5000;
-app.use(express.json());
-app.use(cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-    "http://localhost:5173",
-  'https://cedarclinic.co.ke',
-    'https://www.cedarclinic.co.ke',
-    ];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }));
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`);
-})
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
 
 module.exports = app;
